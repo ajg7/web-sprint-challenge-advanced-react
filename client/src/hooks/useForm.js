5661dec158fb93da0c5ecbe3b39afd3f6044cac1
ajg7 <ajgebara@gmail.com>
2020-09-04 11:01:48 -0500

    created useForm custom hook

--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -2,8 +2,18 @@
 import { useState } from "react";
 
 export const useForm = (key, initialValue) => {
-    const [values, setValues] = setState(initialValue);
+    const [showSuccessMessage, setShowSuccessMessage] = useState(false);
+    const [values, setValues] = useState(initialValue);
+
+    const handleChanges = (e) => {
+        setValues({ ...values, [e.target.name]: e.target.value });
+    };
+    
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setShowSuccessMessage(true);
+    };
 
 
-    return [values]
+    return [showSuccessMessage, values, handleChanges, handleSubmit];
 }
