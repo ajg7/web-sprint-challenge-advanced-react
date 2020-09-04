1542741fc03b5f1a6c42e1dc7518c85925da7109
ajg7 <ajgebara@gmail.com>
2020-09-04 10:47:28 -0500

    created a useForm hook to holds the initialValues of the checkout form

--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -1,1 +1,9 @@
 // write your custom hook here to control your checkout form
+import { useState } from "react";
+
+export const useForm = (key, initialValue) => {
+    const [values, setValues] = setState(initialValue);
+
+
+    return [values]
+}
